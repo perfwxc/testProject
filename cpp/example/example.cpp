0d59efe65ca43b26019a755bc86572f3958e954d
perfwxc <perfwxc@gmail.com>
2020-12-03 10:24:14 +0800

    初始化

---
+++ cpp/example/example.cpp
@@ -0,0 +1,22 @@
+int main(char *a[])
+{
+    max();
+}
+
+int a(int a, char b, string c)
+{
+    int a = 1, b = 2;
+    min(a, b);
+}
+
+int b(int a[], string b[])
+{
+    a();
+    b();
+    return 0;
+}
+
+int c()
+{
+    return 0;
+}
