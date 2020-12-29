50c0a5d5eed38d0da63fcee66569b7d553b5646d
perfwxc <perfwxc@gmail.com>
2020-12-29 20:56:46 +0800

    测试

---
+++ cpp/example/n.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void func1() {
+    cout << 666 << endl;
+}
+
+void func2(int a) {
+    func1();
+    cout << 666 << endl;
+}
+
+void func3(int a, int b) {
+    func1();
+    func2(1);
+    cout << 666 << endl;
+}
+
+int main() {
+    func1();
+    func2(1);
+    func3(1, 2);
+    return 0;
+}
