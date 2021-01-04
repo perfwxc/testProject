396abae982f1b27ed78a1664d1cd60bc79250414
perfwxc <perfwxc@gmail.com>
2021-01-04 22:28:18 +0800

    测试

--- cpp/example/n.cpp
+++ cpp/example/n.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void func1() {
-    cout << 666 << endl;
-}
-
-void func2(int a) {
+void func1()
+{
     func1();
     cout << 666 << endl;
 }
 
-void func3(int a, int b) {
+void func2(int a)
+{
+    func1();
+    cout << 666 << endl;
+}
+
+void func3(int a, int b)
+{
     func1();
     func2(1);
     cout << 666 << endl;
 }
 
-int main() {
+int main()
+{
     func1();
     func2(1);
     func3(1, 2);
